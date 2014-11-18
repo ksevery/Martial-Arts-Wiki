3ebce9de2c79306df632367edc6850a0f74da9cb
ksevery <konstantinsevery@gmail.com>
2014-11-18 16:01:42 +0200

    Initial solution
    Initial comit with Parse.com

---
+++ MartialArtsWiki/MartialArtsWiki/MartialArtsWiki.Shared/ViewModel/MainViewModel.cs
@@ -0,0 +1,11 @@
+using GalaSoft.MvvmLight;
+
+namespace MartialArtsWiki.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
